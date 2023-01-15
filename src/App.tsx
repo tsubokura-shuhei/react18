75bd1c6a8a8418634c99e21186bc577e76f11c8e
tsubokura-shuhei <repeat2639@gmail.com>
2023-01-15 15:42:23 +0900

    section4-15 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { AutoBatchEventHandler } from './components/AutoBatchEventHandler';
 import { AutoBatchOther } from './components/AutoBatchOther';
+import { Transitin } from './components/Transitin';
 
 function App() {
   return (
     <div className="App">
       <AutoBatchEventHandler />
       <AutoBatchOther />
+      <hr />
+      <Transitin />
     </div>
   );
 }
